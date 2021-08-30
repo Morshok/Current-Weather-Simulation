d6b58ffd5064fc0d1c989aa4b69636db688f4353
Morshok <78026935+Morshok@users.noreply.github.com>
2021-08-30 20:33:51 +0200

    Added a daynight cycle script

---
+++ Current Weather Simulation/Assets/Scripts/Daynight Cycle/LightingManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class LightingManager : MonoBehaviour
+{
+    [SerializeField] private Light DirectionalLight;
+    [SerializeField] private LightingPreset preset;
+
+    [SerializeField, Range(0, 24)] private float TimeOfDay;
+
+    private void Update()
+    {
+        if (preset == null)
+            return;
+
+        if(Application.isPlaying)
+        {
+            TimeOfDay += Time.deltaTime;
+            TimeOfDay %= 24;
+        }
+
+        UpdateLighting(TimeOfDay / 24f);
+    }
+
+    private void UpdateLighting(float timePercentage)
+    {
+        RenderSettings.ambientLight = preset.AmbientColor.Evaluate(timePercentage);
+        RenderSettings.fogColor = preset.FogColor.Evaluate(timePercentage);
+
+        if(DirectionalLight != null)
+        {
+            DirectionalLight.color = preset.DirectionalColor.Evaluate(timePercentage);
+            DirectionalLight.transform.rotation = Quaternion.Euler(new Vector3((timePercentage * 360f) - 90f, 180f, 0));
+        }
+    }
+
+    private void OnValidate()
+    {
+        if(DirectionalLight != null)
+            return;
+
+        if (RenderSettings.sun != null)
+            DirectionalLight = RenderSettings.sun;
+        else
+        {
+            Light[] ligths = GameObject.FindObjectsOfType<Light>();
+
+            foreach(Light light in ligths)
+            {
+                if(light.type == LightType.Directional)
+                {
+                    DirectionalLight = light;
+                    return;
+                }
+            }
+        }
+
+    }
+}
