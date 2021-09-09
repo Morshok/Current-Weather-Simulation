fba0799b8bd82a720d987c2ac724bba902a4e462
Morshok <78026935+Morshok@users.noreply.github.com>
2021-09-09 23:38:19 +0200

    Fixed some issues with the suns rotation

--- Current Weather Simulation/Assets/Scripts/Daynight Cycle/LightingManager.cs
+++ Current Weather Simulation/Assets/Scripts/Daynight Cycle/LightingManager.cs
@@ -55,16 +55,30 @@ public class LightingManager : MonoBehaviour
             float fractionalYear = calculateFractionalYear();
             float solarDeclinationAngle = calculateSolarDeclinationAngle(fractionalYear);
             int equationOfTime = calculateEquationOfTime(fractionalYear);
-            int timeOffset = calculateTimeOffset(equationOfTime);
+            float latitude = sphericalCoordinate.getLatitude();
+            float longitude = sphericalCoordinate.getLongitude();
+            int timeOffset = calculateTimeOffset(equationOfTime, longitude);
             float trueSolarTime = calculateTrueSolarTime(timeOffset);
             float solarHourAngle = calculateSolarHourAngle(trueSolarTime);
-            float latitude = sphericalCoordinate.getLatitude();
             float solarZenithAngle = calculateSolarZenithAngle(latitude, solarDeclinationAngle, solarHourAngle);
-            float solarAzimuthAngle = calculateSolarAzimuthAngle(latitude, solarDeclinationAngle, solarHourAngle, solarZenithAngle);
-            float elevationAngle = 90.0f - solarZenithAngle;
+            float solarAzimuthAngle = Mathf.Rad2Deg * calculateSolarAzimuthAngle(latitude, solarDeclinationAngle, solarHourAngle, solarZenithAngle);
+            float elevationAngle = Mathf.Rad2Deg * (Mathf.PI - solarZenithAngle);
+
+            /*
+            Debug.Log("Fractional Year: " + fractionalYear + "\n" +
+                      "Solar Declination Angle: " + solarDeclinationAngle + "\n" +
+                      "Equation of Time: " + equationOfTime + "\n" +
+                      "Latitude: " + latitude + "\n" +
+                      "Longitude: " + longitude + "\n" +
+                      "Time Offset: " + timeOffset + "\n" +
+                      "True Solar Time: " + trueSolarTime + "\n" +
+                      "Solar Hour Angle: " + solarHourAngle + "\n" +
+                      "Solar Zenith Angle: " + Mathf.Rad2Deg * solarZenithAngle + "\n" +
+                      "Solar Azimuth Angle: " + solarAzimuthAngle + "\n");
+            */
 
 
-            DirectionalLight.transform.rotation = Quaternion.Euler(new Vector3(elevationAngle, solarAzimuthAngle, 0));
+            DirectionalLight.transform.localRotation = Quaternion.Euler(new Vector3((timePercentage * 360.0f) - elevationAngle, solarAzimuthAngle, 0.0f));
         }
     }
 
@@ -98,19 +112,19 @@ public class LightingManager : MonoBehaviour
     private float calculateFractionalYear()
     {
         int numDaysInYear = isLeapYear ? 366 : 365;
-        int dayOfYear = System.DateTime.Now.Day;
-        int currentHour = System.DateTime.Now.Hour;
+        int dayOfYear = DateTime.Now.Day;
+        int currentHour = DateTime.Now.Hour;
 
         return ((2 * Mathf.PI) / numDaysInYear) * (dayOfYear - 1 + ((currentHour - 12) / 24));
     }
 
-    //Method for calculating the equation of time
+    //Method for calculating the equation of time, in minutes
     private int calculateEquationOfTime(float fractionalYear)
     {
         return (int)(229.18f * (0.000075f + 0.001868f * Mathf.Cos(fractionalYear)             - 0.032077f * Mathf.Sin(fractionalYear) - 0.014615f * Mathf.Cos(2 * fractionalYear)             - 0.040849f * Mathf.Sin(2 * fractionalYear)));
     }
 
-    //Method for calculating the solar declination angle
+    //Method for calculating the solar declination angle, in radians
     private float calculateSolarDeclinationAngle(float fractionalYear)
     {
         return 0.006918f - 0.399912f * Mathf.Cos(fractionalYear) 
@@ -119,39 +133,46 @@ public class LightingManager : MonoBehaviour
             - 0.002697f * Mathf.Cos(3 * fractionalYear) + 0.00148f * Mathf.Sin(3 * fractionalYear);
     }
     
-    //Method for calculating the time offset
-    private int calculateTimeOffset(int equationOfTime)
+    //Method for calculating the time offset, in minutes
+    private int calculateTimeOffset(int equationOfTime, float longitude)
     {
         int timezoneDifference = DateTime.UtcNow.Hour - DateTime.Now.Hour;
 
-        return (int)(equationOfTime + (4 * sphericalCoordinate.getLongitude()) - (60 * timezoneDifference));
+        return (int)(equationOfTime + (4.0f * longitude) - (60 * timezoneDifference));
     }
 
-    //Method for calculating the true solar time
+    //Method for calculating the true solar time, in minutes
     private float calculateTrueSolarTime(int timeOffset)
     {
         int currentHour = DateTime.Now.Hour;
         int currentMinute = DateTime.Now.Minute;
         int currentSecond = DateTime.Now.Second;
 
-        return ((currentHour * 60) + currentMinute + (currentSecond / 60) + timeOffset);
+        return ((currentHour * 60.0f) + currentMinute + (currentSecond / 60.0f) + timeOffset);
     }
 
-    //Method for calculating the solar hour angle in degrees
+    //Method for calculating the solar hour angle, in radians
     private float calculateSolarHourAngle(float trueSolarTime)
     {
-        return (trueSolarTime / 4) - 180.0f;
+        return Mathf.Deg2Rad * ((trueSolarTime / 4.0f) - 180.0f);
     }
 
+    //Method for calculating the solar zenith angle, in radians
     private float calculateSolarZenithAngle(float latitude, float solarDeclinationAngle, float solarHourAngle)
     {
-        return Mathf.Acos((Mathf.Sin(latitude) * Mathf.Sin(solarDeclinationAngle)) + 
-              (Mathf.Cos(latitude) * Mathf.Cos(solarDeclinationAngle) * Mathf.Cos(solarHourAngle)));
+        float summand1 = Mathf.Sin(Mathf.Deg2Rad * latitude) * Mathf.Sin(solarDeclinationAngle);
+        float summand2 = Mathf.Cos(Mathf.Deg2Rad * latitude) * Mathf.Cos(solarDeclinationAngle) * Mathf.Cos(solarHourAngle);
+
+        return Mathf.Acos(summand1 + summand1);
     }
 
+    //Method for calculating the solar azimuth angle, in radians
     private float calculateSolarAzimuthAngle(float latitude, float solarDeclinationAngle, float solarHourAngle, float solarZenithAngle)
     {
-        return 180.0f - Mathf.Acos(-((Mathf.Sin(latitude) * Mathf.Cos(solarZenithAngle) - Mathf.Sin(solarDeclinationAngle)) / (Mathf.Cos(latitude)*Mathf.Sin(solarZenithAngle))));
+        float nominator = Mathf.Sin((Mathf.Deg2Rad * latitude)) * Mathf.Cos(solarZenithAngle) - Mathf.Sin(solarDeclinationAngle);
+        float denominator = Mathf.Cos((Mathf.Deg2Rad * latitude)) * Mathf.Sin(solarZenithAngle);
+
+        return Mathf.PI - Mathf.Acos(-nominator / denominator); 
     }
     #endregion
 }
